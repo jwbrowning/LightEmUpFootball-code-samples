021aa3a31d395f6ca45ebfe42ed98d04b670ed10
Johnathan Browning <59819871+jwbrowning@users.noreply.github.com>
2023-02-28 16:21:16 -0500

    Add files via upload

---
+++ TeamPersonalities/IDefensePersonality.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDefensePersonality {
+    
+    List<string> PreferredPlayTags();
+    /*
+        dl,db
+    */
+    List<int> PreferredPlayerCounts();
+    List<int> MaximumYoungPlayerCounts();
+    string Type();
+
+}
