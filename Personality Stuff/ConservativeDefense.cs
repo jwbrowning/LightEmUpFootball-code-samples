fbc25edd8f69b2921b783d015a1e6b2c353690c4
Johnathan Browning <59819871+jwbrowning@users.noreply.github.com>
2023-02-28 16:18:08 -0500

    Add files via upload

---
+++ Personality Stuff/ConservativeDefense.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ConservativeDefense : IDefensePersonality {
+
+    public ConservativeDefense() {
+
+    }
+
+    public List<string> PreferredPlayTags() {
+        return new List<string>() {
+            "normal",
+            "prevent"
+        };
+    }
+
+    public List<int> PreferredPlayerCounts() {
+        return new List<int>() {
+            2,4
+        };
+    }
+
+    public List<int> MaximumYoungPlayerCounts() {
+        return new List<int>() {
+            3,5,    7
+        };
+    }
+
+    public string Type() {
+        return "Conservative";
+    }
+
+}
