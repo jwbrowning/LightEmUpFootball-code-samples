bd5105f2bab515ac6d7ce2eca86e70d549ee6452
Johnathan Browning <59819871+jwbrowning@users.noreply.github.com>
2023-02-28 16:15:05 -0500

    Add files via upload

---
+++ Personality Stuff/IOffensePersonality.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IOffensePersonality {
+    
+    float QuarterbackScore(Quarterback qb);
+    List<string> PreferredPlayTags();
+    /*
+        qb,wr,rb,ol
+    */
+    List<int> PreferredPlayerCounts();
+    List<int> MaximumYoungPlayerCounts();
+    string Type();
+    float MaxFieldGoalDistance();
+
+}
